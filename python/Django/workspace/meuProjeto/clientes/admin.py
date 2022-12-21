ce78ec91fb90fa176ff016faa95e1ebe62455ddf
Alex <alexjunior2018@gmail.com>
2022-12-21 13:11:18 -0300

    Meu Primeiro Projeto em Python/Django

---
+++ python/Django/workspace/meuProjeto/clientes/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Empregado, Telefone, CPF, Departamento
+# Register your models here.
+
+admin.site.register(Empregado)
+admin.site.register(Telefone)
+admin.site.register(CPF)
+admin.site.register(Departamento)
