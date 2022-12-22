073469e4aca7c2eca6686b366206d656ea77750a
Alex <alexjunior2018@gmail.com>
2022-12-22 12:17:19 -0300

    Excluindo Arquivos

--- python/Django/workspace/meuProjeto/clientes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Empregado, Telefone, CPF, Departamento
-# Register your models here.
-
-admin.site.register(Empregado)
-admin.site.register(Telefone)
-admin.site.register(CPF)
-admin.site.register(Departamento)
